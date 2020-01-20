5b469f7476ddc7f65ea6e831479b18de883bb5b7
bukirwa99 <angellabukirwa99@gmail.com>
2020-01-20 17:33:16 +0300

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  const hello = 'world'
+  return (
+    <div className="home">
+      <h1>{hello}</h1>
+    </div>
+  );
+}
+
+export default Home;
